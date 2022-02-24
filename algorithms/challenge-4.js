29e082ee2634b437547576d1fdcc3c02c8bb3b3c
stanzealot <stanzealot@gmail.com>
2022-02-24 23:24:24 +0100

    function highestSum implemented

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,14 @@
-function highestSum(x, y) {}
+function highestSum(x, y) {
+    let maxSum;
+    let sumX = 0; let sumY = 0;
+    for(let i=0;i<=x.length-1;i++){
+        sumX = sumX+x[i];
+        sumY = sumY+y[i];      
+
+    }
+    sumX > sumY ?  maxSum =sumX : maxSum = sumY
+    return maxSum;
+}
+
 
 module.exports = highestSum
