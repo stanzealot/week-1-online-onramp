965a988bcd19b52cb856e628243a9560f3de4385
stanzealot <stanzealot@gmail.com>
2022-02-24 23:45:13 +0100

    function replaceDivisible implemented

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,12 @@
-function replaceDivisible(x, y) {}
+function replaceDivisible(x, y) {
+    let newArray = [];
+    for(let i=0;i<=x.length-1;i++){
+        if(x[i]%y === 0){
+            x[i] = "isDivisible"
+        }
+    }
+    return x;
+}
+
 
 module.exports = replaceDivisible
