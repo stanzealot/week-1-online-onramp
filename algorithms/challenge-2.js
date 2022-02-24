1a7baddd263da8135d8a72c4c370756bb4a3a9e5
stanzealot <stanzealot@gmail.com>
2022-02-24 22:26:55 +0100

    function divisible implemented

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,13 @@
-function divisible(a) {}
+function divisible(a) {
+    count = 0
+    a.forEach(element => {
+        if(element%2 === 0 && element > 15){
+            count++
+        }
+    });
+    return count;
+}
+
+
 
 module.exports = divisible
