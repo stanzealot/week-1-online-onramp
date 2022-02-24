683b4f7085a1ee8b25d89063227f518d916ce9e2
stanzealot <stanzealot@gmail.com>
2022-02-24 22:40:31 +0100

    function isDivisbleByN implemented

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,12 @@
-function isDivisbleByN(arr, n) {}
+function isDivisbleByN(arr, n) {
+    let newArray = [];
+    arr.forEach(element => {
+        if(element%n === 0){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
+
 
 module.exports = isDivisbleByN
