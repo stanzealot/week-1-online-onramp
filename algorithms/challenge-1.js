786c043b1c876fd51af690f01579a9bab3385555
stanzealot <stanzealot@gmail.com>
2022-02-24 22:18:56 +0100

    function noOfElements implemented

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,15 @@
-function noOfElements(x, y, z) {}
+function noOfElements(x, y, z) {
+    count = 0;
+    x.forEach(element => {
+        if(element >= y && element <= z){
+            count++
+        }
+    });
+    return count;
+}
+
+
+//console.log(noOfElements([4,6,2,6,7,7], 0, 7));
+
 
 module.exports = noOfElements
